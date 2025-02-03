7a9c7261b9a624980c59dc3a0c528f5cb51c03cc
brunornsousa92 <brunornsousa@gmail.com>
2025-02-03 16:48:28 -0300

    update ResultadoEscolar.java file

--- meu-programa/src/je09_controle_fluxo/condicional/ResultadoEscolar.java
+++ meu-programa/src/je09_controle_fluxo/condicional/ResultadoEscolar.java
@@ -2,19 +2,35 @@ package je09_controle_fluxo.condicional;
 
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 6;
+//        int nota = 6;
+//
+//        if (nota >= 7){
+//            System.out.println("Aprovado!");
+//        } else if (nota >= 5 && nota < 7) {
+//            fluxoIntermediario();
+//        } else{
+//            System.out.println("Reprovado!");
+//        }
 
-        if (nota >= 7){
-            System.out.println("Aprovado!");
-        } else if (nota >= 5 && nota < 7) {
-            fluxoIntermediario();
-        } else{
-            System.out.println("Reprovado!");
-        }
-    }
-    static void fluxoIntermediario(){
-        // Faria alguma coisa
-        System.out.println("Recuperação!");
-        System.out.println("Estude um pouco mais");
+//        static void fluxoIntermediario(){
+//            // Faria alguma coisa
+//            System.out.println("Recuperação!");
+//            System.out.println("Estude um pouco mais");
+//        }
+
+        // Atribuição Ternária
+        // Cenário 1
+
+//        int nota = 7;
+//        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+//        System.out.println(resultado);
+
+        // Cenario 2
+
+        int nota = 8;
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
+
+
     }
 }
