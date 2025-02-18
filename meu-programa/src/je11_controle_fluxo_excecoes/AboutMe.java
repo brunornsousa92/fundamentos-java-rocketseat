aca6c99b25e61e0c7ea1f92b200b3334c3e1eb2c
brunornsousa92 <brunornsousa@gmail.com>
2025-02-18 09:33:25 -0300

    finished exercise AboutMe.java file

--- meu-programa/src/je11_controle_fluxo_excecoes/AboutMe.java
+++ meu-programa/src/je11_controle_fluxo_excecoes/AboutMe.java
@@ -16,53 +16,55 @@ public class AboutMe {
         double altura = 0;
         boolean alturaValida = false;
 
-        try {
-            System.out.println("Digite seu nome");
-            nome = scanner.nextLine().trim();
-            if (nome.isEmpty()) {
-                throw new IllegalArgumentException("O nome não pode estar vazio.");
-            }
-
-            System.out.println("Digite seu sobrenome");
-            sobrenome = scanner.nextLine().trim();
-            if (sobrenome.isEmpty()){
-                throw new IllegalArgumentException("O sobrenome não pode star vazio.");
-            }
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-
-        while (!idadeValida){
+        while(nome.isEmpty() || sobrenome.isEmpty()){
             try {
-                System.out.println("Digite sua idade:");
-                idade = scanner.nextInt();
-                if (idade < 0){
-                    System.out.println("Erro: A idade não pode ser negativa. Tente novamente");
-                    idadeValida = false;
-                }else {
-                    idadeValida = true;
+                if (nome.isEmpty()){
+                    System.out.println("Digite seu nome");
+                    nome = scanner.nextLine().trim();
+                    if (nome.isEmpty()) {
+                        throw new IllegalArgumentException("O nome não pode estar vazio.");
+                    }
                 }
-            } catch (InputMismatchException e){
-                System.out.println("Erro: A idade deve ser um número inteiro. Tente novamente");
-                scanner.next();
+                if (sobrenome.isEmpty()){
+                    System.out.println("Digite seu sobrenome");
+                    sobrenome = scanner.nextLine().trim();
+                    if (sobrenome.isEmpty()){
+                        throw new IllegalArgumentException("O sobrenome não pode star vazio.");
+                    }
+                }
+                while (!idadeValida){
+                    try {
+                        System.out.println("Digite sua idade:");
+                        idade = scanner.nextInt();
+                        if (idade < 0){
+                            System.out.println("Erro: A idade não pode ser negativa. Tente novamente");
+                            idadeValida = false;
+                        }else {
+                            idadeValida = true;
+                        }
+                    } catch (InputMismatchException e){
+                        System.out.println("Erro: A idade deve ser um número inteiro. Tente novamente");
+                        scanner.next();
+                    }
+                }
+                while(!alturaValida){
+                    try {
+                        System.out.println("Digite sua altura (use ponto como separador decimal, ex: 1.75):");
+                        altura = scanner.nextDouble();
+                        alturaValida = true;
+                    }catch (InputMismatchException e){
+                        System.out.println("Erro: A altura deve ser um número decimal com ponto (.) como separador. Tente novamente.");
+                        scanner.next();
+                    }
+                }
+                // Imprimindo os dados obtidos pelo usuário
+                System.out.println("Olá, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
+                System.out.println("Tenho " + idade + " anos ");
+                System.out.println("Minha altura é " + altura + "cm ");
+                scanner.close();
+            }catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
             }
-        }
-
-        while(!alturaValida){
-            try {
-                System.out.println("Digite sua altura (use ponto como separador decimal, ex: 1.75):");
-                altura = scanner.nextDouble();
-                alturaValida = true;
-            }catch (InputMismatchException e){
-                System.out.println("Erro: A altura deve ser um número decimal com ponto (.) como separador. Tente novamente.");
-                scanner.next();
-            }
-        }
-
-        // Imprimindo os dados obtidos pelo usuário
-        System.out.println("Olá, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
-        System.out.println("Tenho " + idade + " anos ");
-        System.out.println("Minha altura é " + altura + "cm ");
-        scanner.close();
+    }
     }
 }
