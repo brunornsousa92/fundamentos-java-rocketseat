33b36d5801512c0cde51c911584b24518377c9da
brunornsousa92 <brunornsousa@gmail.com>
2025-03-27 17:24:05 -0300

    add ComputadorBruninho.java and MSNMessenger.java class

---
+++ meu-programa/src/je14_pilares_poo/ComputadorBruninho.java
@@ -0,0 +1,11 @@
+package je14_pilares_poo;
+
+public class ComputadorBruninho {
+    public static void main(String[] args) {
+        MSNMessenger msnMessenger = new MSNMessenger();
+        msnMessenger.enviarMensagem();
+        msnMessenger.recebendoMensagem();
+//        msnMessenger.validarConectadoInternet();
+//        msnMessenger.salvarHistoricoMensagem();
+    }
+}
