f8e762d81f0b46e2b421e93aafae5bb9fcb725bf
brunornsousa92 <brunornsousa@gmail.com>
2025-04-03 11:29:10 -0300

    update ComputadorBruninho.java class

--- meu-programa/src/je14_pilares_poo/ComputadorBruninho.java
+++ meu-programa/src/je14_pilares_poo/ComputadorBruninho.java
@@ -2,18 +2,28 @@ package je14_pilares_poo;
 
 public class ComputadorBruninho {
     public static void main(String[] args) {
-        MSNMessenger msnMessenger = new MSNMessenger();
-        msnMessenger.enviarMensagem();
-        msnMessenger.recebendoMensagem();
+//        MSNMessenger msnMessenger = new MSNMessenger();
+//        msnMessenger.enviarMensagem();
+//        msnMessenger.recebendoMensagem();
 //        msnMessenger.validarConectadoInternet();
 //        msnMessenger.salvarHistoricoMensagem();
+//        FacebookMessenger facebookMessenger = new FacebookMessenger();
+//        facebookMessenger.enviarMensagem();
+//        facebookMessenger.recebendoMensagem();
+//        Telegram telegram = new Telegram();
+//        telegram.enviarMensagem();
+//        telegram.recebendoMensagem();
+        SistemaMensagemInstantanea smi = null;
+        String appEscolhido = "msn";
 
-        FacebookMessenger facebookMessenger = new FacebookMessenger();
-        facebookMessenger.enviarMensagem();
-        facebookMessenger.recebendoMensagem();
+        if (appEscolhido.equals("msn"))
+            smi = new MSNMessenger();
+        else if (appEscolhido.equals("fbm"))
+            smi = new FacebookMessenger();
+        else if (appEscolhido.equals("tlg"))
+            smi = new Telegram();
 
-        Telegram telegram = new Telegram();
-        telegram.enviarMensagem();
-        telegram.recebendoMensagem();
+        smi.enviarMensagem();
+        smi.recebendoMensagem();
     }
 }
